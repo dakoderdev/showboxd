b92195064dd7664db17ec87e6b3f386f824f6880
dakodev <123116563+dakoderdev@users.noreply.github.com>
2026-03-31 04:17:19 -0300

    added and linked shows table correctly for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import Image from "next/image";
 
 export default async function Page() {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
-  const { data: todos } = await supabase.from("todos").select();
+  const { data: shows } = await supabase.from("shows").select();
 
   return (
     <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
+      {shows?.map((show, index) => (
+        <li key={show.id ?? `${show.name}-${index}`}>
+          <h2>{show.name}</h2>
+          <Image src={show.img_vertical} alt={show.name} width={200} height={300} />
+        </li>
       ))}
     </ul>
   );
